3a16f7af5c7060ef442aa0cd4d69a26e74d920e2
Sakib Khan <npcsakib@gmail.com>
2026-02-12 20:33:17 +0600

    Admim Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin/dashboard', [AdminDashboardController
+::class,'dashboard'])->name('admin.dashboard')->middleware('auth');
